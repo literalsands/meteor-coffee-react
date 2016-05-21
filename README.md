fc3ea7aa23256257056d79e520fa8a176a727643
Austin New <literalsands@gmail.com>
2016-05-21 18:38:24 -0400

    first

---
+++ README.md
@@ -0,0 +1,5 @@
+# Coffee React for Meteor
+
+[Coffee React](https://github.com/jsdf/coffee-react) + [Meteor's CoffeeCompiler](https://github.com/meteor/meteor/tree/devel/packages/coffeescript)
+
+
---
+++ package.js
@@ -0,0 +1,17 @@
+Package.describe({
+  summary: 'Use Coffee React Transform to compile CJSX.',
+  version: '5.0.0',
+  name: 'literalsands:coffee-react'
+});
+Package.registerBuildPlugin({
+  name: 'compileCJSX',
+  use: ['caching-compiler', 'ecmascript'],
+  sources: ['compile-cjsx.js'],
+  npmDependencies: {
+    "source-map": "0.5.3",
+    "coffee-react": "5.0.0"
+  }
+});
+Package.onUse(function (api) {
+  api.use('isobuild:compiler-plugin@1.0.0');
+});
